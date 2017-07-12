766a587406337a5df8db8318b1cd9e91bfb02eea
Alex Nicita <an2582@columbia.edu>
2017-07-12 13:38:15 -0700

    heeeeeeeeere you go

--- scripts.js
+++ scripts.js
@@ -1,21 +1,15 @@
 
 
 /* trying to make home image fade away as you scroll down */
-$(document).scroll(function () {
-  var scrollTop = $(document).scrollTop();
-  var height = $(document).height();
-  $('.home_image').css({
-    'opacity':((height - scrollTop) / height)
+$(window).scroll(function () {
+  var scrollTop = $(window).scrollTop();
+  var height = $(window).height();
+  $('#saturn-pic').css({
+    'opacity':((height - scrollTop) /(height))
   })
 })
 
 /* experimenting */
-$(document).ready(function() {
-  $('p').click(function(){
-    $(this).hide()
-  })
-})
-
 $(document).ready(function() {
   $('#overview-title').click(function(){
     $('#overview-title').hide()
