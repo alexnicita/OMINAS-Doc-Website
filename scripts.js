1b2c4a7399bae425831fc867b943ce93b040ee88
Alex Nicita <an2582@columbia.edu>
2017-07-25 14:20:32 -0700

    New flow for home screen. New button UI.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-
+/* defaults when page is loaded */
+$(document).ready(function () {
+  $('.all-buttons').hide()
+})
 
 /* home image fade away as you scroll down */
 $(window).scroll(function () {
@@ -10,58 +13,41 @@ $(window).scroll(function () {
   })
 })
 
-/* welcome div fade in as you scroll down */
-
-/* this ain't working though */
-// $(document).ready(function () {
-//   $('.welcome').css({
-//     'opacity': 0
-//   })
-// })
-
-/* neither is this */
-// $(window).scroll(function () {
-//   if ($(window).scrollTop = 0) {
-//     'opacity': 0
-//   }
-// })
-
+// opacity gradually increases as you scroll
 $(window).scroll(function () {
     var bottom_of_welcome = $('.welcome').offset().top + $('.welcome').outerHeight();
     var bottom_of_window = $(window).scrollTop() + $(window).height();
-    $('.welcome').css({
-      'opacity': (4*(bottom_of_window - bottom_of_welcome))/(bottom_of_welcome)
-    })
+
+    if ((bottom_of_window - bottom_of_welcome) < (($(window).height())/(2.5))) {
+      $('.welcome').css({
+        'opacity': (4*(bottom_of_window - bottom_of_welcome))/(bottom_of_welcome)
+      })
+    }
 })
 
+// click to fade out welcome and fade in all buttons
+$(document).ready(function () {
+  $('#enter-button').click(function () {
+    $('.welcome').fadeOut(750)
+    $('.all-buttons').delay(1000).fadeIn(750)
+  })
+})
+
+// below is the old home page that didn't have a click to enter on it
+
+// /* welcome fade in, fade out, and button fade in on scroll */
 // $(window).scroll(function () {
-//     var bottom_of_obj = $(this).offset().top + $(this).outerHeight(;
+//     var bottom_of_welcome = $('.welcome').offset().top + $('.welcome').outerHeight();
 //     var bottom_of_window = $(window).scrollTop() + $(window).height();
-//     if (bottom_of_window > bottom_of_obj) {
-//         $('#welcome-title').animate({'opacity': '1'}, 750)
-//         $('#welcome-subtitle').animate({'opacity': '1'}, 750)
-//     }
-// })
-
-// $(window).scroll(function () {
-//   var bottomOfObj = $(this).offset().top + $(this).outerHeight(true);
-//   /* bottom of window is distance from top of screen plus the height of the current window */
-//   var bottomOfWindow = $(window).scrollTop() + $(window).height();
-//   /* if window's bottom is further from the top of the page than the object's bottom, start fading in */
-//   if (bottomOfWindow > bottomOfObj) {
-//     $('.welcome').css({
 //
-//     })
-//   }
-// })
-
-
-
-/* experimenting */
-
-/* hides #overview-title on click */
-// $(document).ready(function() {
-//   $('#overview-title').click(function(){
-//     $('#overview-title').hide()
-//   })
+//     /* issue: display is glitchy if welcome has faded out, you scroll to the top of the page, and then back down again */
+//     if ((bottom_of_window - bottom_of_welcome) > (($(window).height())/(2.5))) {
+//       $('.welcome').fadeTo(750, 0)
+//       $('.svg-wrapper').delay(1000).fadeTo(750, 1)
+//     }
+//     else {
+//       $('.welcome').css({
+//         'opacity': (5*(bottom_of_window - bottom_of_welcome))/(bottom_of_welcome)
+//       })
+//     }
 // })
